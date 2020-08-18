fd90d30ad9c37c12262f7c73945bf9f4a71b5854
vattcarter7 <vattcarter@gmail.com>
2020-08-18 15:37:27 +0700

    added validateRequest middleware

---
+++ booking-api/src/middleware/validateRequest.js
@@ -0,0 +1,10 @@
+const { validationResult } = require('express-validator');
+
+module.exports = function (req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty())
+    return res.status(400).send({ errors: errors.array() });
+
+  next();
+};
