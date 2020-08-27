0d52b36538c3c1497c7ac6dee70be3f9946d1f6e
vattcarter7 <vattcarter@gmail.com>
2020-08-27 17:24:47 +0700

    finished create table user category product and added some data

--- booking-api/src/db/dropTable.sql
+++ booking-api/src/db/dropTable.sql
@@ -1,3 +1,9 @@
-DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS category CASCADE;
 DROP TABLE IF EXISTS product CASCADE;
+
+
+-- Delete only data of the table
+DELETE FROM users;
+DELETE FROM category;
+DELETE FROM product;
