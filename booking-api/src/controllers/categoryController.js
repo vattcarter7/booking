59424a8cf5d2ef1dc8747459ff1022e640b500ac
vattcarter7 <vattcarter@gmail.com>
2020-08-29 11:09:00 +0700

    implemented add category

--- booking-api/src/controllers/categoryController.js
+++ booking-api/src/controllers/categoryController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../helpers/errorResponse');
 
 // @desc      get all categories
-// @route     POST /api/v1/category/all
+// @route     GET /api/v1/category/all
 // @access    Public
 exports.getCategories = asyncHandler(async (req, res, next) => {
   const textQuery = `SELECT * FROM category`;
@@ -13,5 +13,21 @@ exports.getCategories = asyncHandler(async (req, res, next) => {
     success: true,
     results: rows,
     size: rows.length
+  });
+});
+
+// @desc      add a category
+// @route     POST /api/v1/category
+// @access    Private/Admin
+exports.addCategory = asyncHandler(async (req, res, next) => {
+  const insertQuery = `INSERT INTO category (name) VALUES ($1) returning *`;
+  const param = [req.body.name];
+
+  const { rows } = await db.query(insertQuery, param);
+  if (!rows[0])
+    return next(new ErrorResponse('Unable to create category', 400));
+  res.status(201).json({
+    success: true,
+    results: rows[0]
   });
 });
--- booking-api/src/routes/categoryRoute.js
+++ booking-api/src/routes/categoryRoute.js
@@ -2,7 +2,22 @@ const express = require('express');
 const { check } = require('express-validator');
 const router = express.Router();
 
-const { getCategories } = require('../controllers/categoryController');
+const {
+  getCategories,
+  addCategory
+} = require('../controllers/categoryController');
+const validateRequest = require('../middleware/validateRequest');
+
+router.post(
+  '/add',
+  [
+    check('name')
+      .isLength({ min: 2 })
+      .withMessage('a valid category name is required'),
+    validateRequest
+  ],
+  addCategory
+);
 
 router.get('/all', getCategories);
 
