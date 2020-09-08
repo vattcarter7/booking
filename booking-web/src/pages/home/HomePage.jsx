4ec3555edcb7954bee306c3fa2991a3d9d728af1
vattcarter7 <vattcarter@gmail.com>
2020-09-09 01:02:30 +0700

    implemented category

--- booking-web/src/pages/home/HomePage.jsx
+++ booking-web/src/pages/home/HomePage.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     margin: '15px 15px'
   },
-  paper: {
+  leftGridContainer: {
+    display: 'flex',
+    flexDirection: 'column',
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary
@@ -22,10 +25,13 @@ export default function FullWidthGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={3} sm={3}>
-          <Paper className={classes.paper}>categories</Paper>
+          <Paper className={classes.leftGridContainer}>
+            <Button>category</Button>
+            <Button>category</Button>
+          </Paper>
         </Grid>
         <Grid item xs={9} sm={9}>
-          <Paper className={classes.paper}>products</Paper>
+          products
         </Grid>
       </Grid>
     </div>
