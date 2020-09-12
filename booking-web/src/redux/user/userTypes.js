cd7e08e09ce9f41ae6ba20a6706fdfcd46250ecc
vattcarter7 <vattcarter@gmail.com>
2020-09-12 15:03:53 +0700

    added user types

--- booking-web/src/redux/user/userTypes.js
+++ booking-web/src/redux/user/userTypes.js
@@ -1,1 +1,4 @@
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const LOGIN_USER = 'LOGIN_USER';
+export const REGISTER_USER = 'REGISTER_USER';
+export const AUTH_USER = 'AUTH_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
