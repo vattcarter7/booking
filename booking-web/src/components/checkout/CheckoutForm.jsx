0c2350a9f6c0d0beb6ae6ca7c144bdb520dfb6e9
vattcarter7 <vattcarter@gmail.com>
2020-09-24 01:42:39 +0700

    added error message for checkout form with stripe

--- booking-web/src/components/checkout/CheckoutForm.jsx
+++ booking-web/src/components/checkout/CheckoutForm.jsx
@@ -1,15 +1,34 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { PURCHASE_URL } from '../../utils/misc';
 import { successBuyAction } from '../../redux/cart/cartAction';
 
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    alignItems: 'center',
+    marginTop: 50,
+    width: 350
+  },
+  payBtn: {
+    marginTop: 30,
+    marginBottom: 20
+  },
+  error: {
+    color: 'red'
+  }
+}));
 
 const CheckoutForm = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const [processing, setProcessing] = useState(false);
+  const [checkoutError, setCheckoutError] = useState();
 
   const stripe = useStripe();
   const elements = useElements();
@@ -38,29 +57,47 @@ const CheckoutForm = () => {
     }
   };
 
+  const cardElementOption = {
+    style: {
+      base: {
+        fontSize: '16px',
+        color: '#424770',
+        '::placeholder': {
+          color: '#aab7c4'
+        }
+      },
+      invalid: {
+        color: '#9e2146'
+      },
+      complete: {
+        iconColor: 'green'
+      }
+    },
+    hidePostalCode: true,
+    iconStyle: 'solid'
+  };
+
+  const handleCardDetailsChange = (ev) => {
+    ev.error ? setCheckoutError(ev.error.message) : setCheckoutError();
+  };
+
   return (
-    <div style={{ width: '300px', alignItems: 'center', margin: '50px' }}>
+    <div className={classes.root}>
       <form onSubmit={handleSubmit}>
         <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: '16px',
-                color: '#424770',
-                '::placeholder': {
-                  color: '#aab7c4'
-                }
-              },
-              invalid: {
-                color: '#9e2146'
-              }
-            },
-            hidePostalCode: true
-          }}
+          options={cardElementOption}
+          onChange={handleCardDetailsChange}
         />
-        <button type='submit' disabled={!stripe || processing}>
+        <button
+          className={classes.payBtn}
+          type='submit'
+          disabled={!stripe || processing}
+        >
           {!processing ? 'Pay Now' : 'Proccessing...'}
         </button>
+        {checkoutError && (
+          <Typography className={classes.error}>{checkoutError}</Typography>
+        )}
       </form>
     </div>
   );
