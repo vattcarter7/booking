00788e9fc7f7ec64b7b2db7ba955872086214388
vattcarter7 <vattcarter@gmail.com>
2020-09-24 22:56:33 +0700

    removed unused state

--- booking-web/src/components/cart/Cart.jsx
+++ booking-web/src/components/cart/Cart.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import CartItem from './CartItem';
 
 const Cart = () => {
-  const { cartItems, loading } = useSelector((state) => state.cart);
-
   return <CartItem />;
 };
 
