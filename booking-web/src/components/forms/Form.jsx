c7a5d471a80126c76cdfccc3fb73da56068164ef
vattcarter7 <vattcarter@gmail.com>
2020-09-27 16:30:32 +0700

    AppForm

---
+++ booking-web/src/components/forms/Form.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import { Formik } from 'formik';
+
+const AppForm = ({ initialValues, onSubmit, validationSchema, children }) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {() => <Fragment>{children}</Fragment>}
+    </Formik>
+  );
+};
+
+export default AppForm;
