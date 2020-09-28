f03ce17286b7002850e77cbaef36d177252f813e
vattcarter7 <vattcarter@gmail.com>
2020-09-28 13:38:31 +0700

    finished implementing login form with formik

--- booking-web/src/components/auth/Login.jsx
+++ booking-web/src/components/auth/Login.jsx
@@ -6,24 +6,99 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import * as Yup from 'yup';
 
-// import { Form, FormField, ErrorMessage, SubmitButton } from '../forms';
 import { login } from '../../redux/user/userAction';
 import { DRAW_WIDTH } from '../../utils/misc';
 
-// const validationSchema = Yup.object().shape({
-//   email: Yup.string().required().email().label('Email'),
-//   password: Yup.string().required().min(4).label('Password')
-// });
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .required('Email is required')
+    .email('Please provide a valid email'),
+  password: Yup.string().required('Password is required')
+});
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+    flexDirection: 'column',
     marginTop: '30px',
+    justifyContent: 'center',
+    alignItems: 'center',
     flexGrow: 1,
     padding: theme.spacing(3),
     [theme.breakpoints.up('sm')]: {
       marginLeft: DRAW_WIDTH,
       flexShrink: 0
     }
+  },
+  formContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: 430,
+    height: 'auto',
+    backgroundColor: '#eff1f9',
+    padding: 15,
+    marginTop: 20,
+    fontWeight: 300,
+    paddingLeft: 57,
+    paddingRight: 57
+  },
+  formFieldContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '350px',
+    padding: 20
+  },
+  formField: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 15
+  },
+  input: {
+    width: '100%',
+    height: 35,
+    border: '1px solid #d6d7db',
+    paddingLeft: 10,
+    marginBottom: 20,
+    background: '#f5f6f7',
+    outline: 'none',
+    textAlign: 'center'
+  },
+  label: {
+    fontWeight: 400,
+    fontSize: 15,
+    color: 'grey',
+    marginRight: 'auto',
+    marginLeft: 'auto'
+  },
+  button: {
+    cursor: 'pointer',
+    backgroundColor: '#1a3be0',
+    color: 'white',
+    border: 'none',
+    padding: 10,
+    fontSize: 16,
+    outline: 'none'
+  },
+  buttonDisabled: {
+    cursor: 'not-allowed',
+    backgroundColor: '#798df2',
+    color: 'white',
+    border: 'none',
+    padding: 10,
+    fontSize: 16,
+    outline: 'none'
+  },
+  error: {
+    alignItems: 'center',
+    alignContent: 'center',
+    color: 'red',
+    fontSize: 12,
+    height: 5,
+    marginTop: -17
   }
 }));
 
@@ -32,97 +107,88 @@ const Login = () => {
   const dispatch = useDispatch();
   const { isAuthenticated } = useSelector((state) => state.user);
 
-  // const [formData, setFormData] = useState({
-  //   email: '',
-  //   password: ''
-  // });
-
-  const [loginFailed, setLoginFailed] = useState(false);
-
-  // const { email, password } = formData;
-
-  // const onChange = (e) =>
-  //   setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  // const handleSubmit = (e) => {
-  //   try {
-  //     e.preventDefault();
-  //     dispatch(login({ email, password }));
-  //     setLoginFailed(false);
-  //   } catch (error) {
-  //     setLoginFailed(true);
-  //   }
-  // };
-
   if (isAuthenticated) {
     return <Redirect to='/dashboard' />;
   }
 
   return (
     <div className={classes.root}>
-      <h1 className='large text-primary'>Sign In</h1>
-      <p className='lead'>
-        <i className='fas fa-user'></i> Sign Into Your Account
-      </p>
+      <div className={classes.formContainer}>
+        <h1 style={{ color: 'grey' }}>Sign In</h1>
+        <h3 style={{ color: 'grey' }}> Sign Into Your Account</h3>
 
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validationSchema={Yup.object({
-          email: Yup.string()
-            .required('Email is required')
-            .email('Please provide a valid email'),
-          password: Yup.string().required('Password is required')
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          dispatch(login(values));
-          setSubmitting(false);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          isValid
-        }) => (
-          <form onSubmit={handleSubmit} autoComplete='off'>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                width: '200px',
-                padding: 20
-              }}
-            >
-              <input
-                type='email'
-                name='email'
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              {errors.email && touched.email && errors.email}
-              <input
-                type='password'
-                name='password'
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-              {errors.password && touched.password && errors.password}
-              <button type='submit' disabled={!isValid || isSubmitting}>
-                Submit
-              </button>
-            </div>
-          </form>
-        )}
-      </Formik>
-      <p className='my-1'>
-        Don't have an account? <Link to='/register'>Register</Link>
-      </p>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            dispatch(login(values));
+            setSubmitting(false);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            isValid
+          }) => (
+            <form onSubmit={handleSubmit} autoComplete='off'>
+              <div className={classes.formFieldContainer}>
+                <div className={classes.formField}>
+                  <label className={classes.label} for='email'>
+                    Email
+                  </label>
+                  <input
+                    className={classes.input}
+                    type='email'
+                    name='email'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <div className={classes.error}>
+                    {errors.email && touched.email && errors.email}
+                  </div>
+                </div>
+
+                <div className={classes.formField}>
+                  <label className={classes.label} for='password'>
+                    Password
+                  </label>
+                  <input
+                    className={classes.input}
+                    type='password'
+                    name='password'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                  <div className={classes.error}>
+                    {errors.password && touched.password && errors.password}
+                  </div>
+                </div>
+                <button
+                  className={
+                    !isValid || isSubmitting
+                      ? classes.buttonDisabled
+                      : classes.button
+                  }
+                  type='submit'
+                  disabled={!isValid || isSubmitting}
+                >
+                  SIGN IN
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
+        <h4 style={{ color: 'grey' }}>
+          Don't have an account? <Link to='/register'>Register</Link>
+        </h4>
+      </div>
     </div>
   );
 };
