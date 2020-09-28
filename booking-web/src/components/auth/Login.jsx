1042b764415730a97bd5ece525313987c2370ef0
vattcarter7 <vattcarter@gmail.com>
2020-09-28 10:27:54 +0700

    removed comment on formik

--- booking-web/src/components/auth/Login.jsx
+++ booking-web/src/components/auth/Login.jsx
@@ -74,10 +74,6 @@ const Login = () => {
         onSubmit={(values, { setSubmitting }) => {
           dispatch(login(values));
           setSubmitting(false);
-          // setTimeout(() => {
-          //   alert(JSON.stringify(values, null, 2));
-          //   setSubmitting(false);
-          // }, 400);
         }}
       >
         {({
